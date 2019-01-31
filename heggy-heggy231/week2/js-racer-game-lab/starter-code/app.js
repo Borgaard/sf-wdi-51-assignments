91407b99a1472e42479c51bce0cb93707bc5fdd5
Heggyhere <heggyy@gmail.com>
2019-01-30 18:23:36 -0800

    add html structure

---
+++ heggy-heggy231/week2/js-racer-game-lab/starter-code/app.js
@@ -0,0 +1,11 @@
+/*
+[x] Start simple! Ask yourself what the MVP of this project is.
+[] Create HTML and CSS files that create one red square div and one blue, lined up on the left side of the screen.
+- Add a keydown event listener that console logs "blue move" or "red move."
+- Replace that console log with JavaScript that moves the red or blue div a little to the right.
+- A helpful jQuery method to look into: [offset()](http://api.jquery.com/offset/).
+- Run with it from there!
+- If you are stuck on a problem, use pseudocode to break it down into smaller, more solvable problems.
+- If you need some ideas on where to start, watch this [video](https://www.youtube.com/watch?v=QVSwX98kKFs).
+
+*/
