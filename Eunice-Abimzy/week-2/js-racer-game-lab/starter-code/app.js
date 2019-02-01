c7138bd592dc5f9fef98e50d369bb1a4586b706d
Abimzy <eabiemo@gmail.com>
2019-01-31 22:45:50 -0800

    Initial draft of racer game logic

---
+++ Eunice-Abimzy/week-2/js-racer-game-lab/starter-code/app.js
@@ -0,0 +1,26 @@
+//Game only moves with click of a button
+//To-do:
+//Use the keyword
+
+//Use global variables to keep the score and increment on each click
+//find element by classnames
+
+//Use keycode for selecting click functions
+//Left key for clicking= tab(keycode-9)
+//Right key for clicking= shift(keycode-16)
+//Use e.keyword to designate keycode
+
+//Element selectors for players:
+ var player1 = document.querySelector(".player1");
+ var player2 = document.querySelector(".player2");
+
+ //Adding event listeners
+ player1.addEventListener("click", movePlayer);
+ function movePlayer (){
+  if (e.keyCode === 9){
+    margin-left
+  }
+
+ }
+
+
