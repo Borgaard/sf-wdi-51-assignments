363855617c7a8d6af88e9275cd81f9f3fc8bb413
lvmor <lvm.lspa@gmail.com>
2019-01-31 16:24:50 -0800

    Submitted js-objects hw week 2

---
+++ lourdesmorales-lvmor/week-2/js-racer-game-lab-master/starter-code/app.js
@@ -0,0 +1,13 @@
+var div = document.getElementById("blueDiv");
+function blue () {
+  div.set Attribute("id", "blueDiv");
+}
+
+function yellow () {
+  div.set Attribute("id", "yellowDiv");
+}
+
+type="text/javascript">
+    document.getElementById("myButton").onclick = function () {
+        location.href = www.git.generalassemb.ly/sf-wdi-51/js-racer-game-lab;
+    };
