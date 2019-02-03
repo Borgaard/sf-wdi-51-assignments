37dde829ecb797a6b07a3923afb9a5e499bc2752
Rhea Roy <rroy.rhea@gmail.com>
2019-02-03 13:15:35 -0800

    Functioning supermarket website and no declared winner racer game

--- rhea-rhear0yishere/week-2/js-racer-game-lab-master/hw/js/app.js
+++ rhea-rhear0yishere/week-2/js-racer-game-lab-master/hw/js/app.js
@@ -1,29 +1,56 @@
-document.querySelector('.button5').addEventListener('click', box1);
-document.querySelector('.button5').addEventListener('click', box2);
-
-function box1(){
-
- var random = Math.floor(Math.random() * 10);
- var randomMultiply = random* 166
-
- document.querySelector('.box1').style.left= randomMultiply+("px")
+// document.querySelector('.button5').addEventListener('click', box1);
+// document.querySelector('.button5').addEventListener('click', box2);
 
 
-}
 
 
-function box2(){
+var current1 = 10;
+var current2 = 10;
+document.querySelector('.box1').style.left= current1+("px")
+document.querySelector('.box2').style.left= current2+("px")
 
- var random = Math.floor(Math.random() * 10);
- var randomMultiply = random* 166
+document.onkeydown = checkKey;
+function checkKey(e) {
 
-    document.querySelector('.box2').style.left= randomMultiply+("px")
-      if (random===9) {
-      alert("BLUE BOX WINNER");
+
+
+    e = e || window.event;
+
+  for(x=0; x<=400; x+=400){
+    if (e.keyCode == '38') {
+        var move1 = 10+current1;
+        current1 = move1
+        document.querySelector('.box1').style.left= current1+("px")
+
+      }
+    else if (e.keyCode == '90') {
+       var move2 = 10+current2;
+        current2 = move2
+        document.querySelector('.box2').style.left= current2+("px")
     }
 
 
+  }
 }
+// current1=0
+// while(current1<1300){
+//   document.onkeydown = checkKey;
+//   if(current1=1200){
+//       console.log("winner")
+
+
+// }
+// current1+=50;
+// }
+
+
+
+
+
+
+
+
+
 
 
 
