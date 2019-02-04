deee0bc383702ae30809d1ca430849234bf7bff3
Heggyhere <heggyy@gmail.com>
2019-02-04 07:05:12 -0800

    using string literal  to get the img info

--- heggy-heggy231/week2/week-two-project/draft2/js/app.js
+++ heggy-heggy231/week2/week-two-project/draft2/js/app.js
@@ -26,8 +26,40 @@
     //  I am looping thru the array.  as click anyone of cart icons > event will fire
     btn.addEventListener('click', function (event) {
       // event.target will let us know what element we are clicking on
-      console.log(event.target);
-    })
+      // notice this will also give me the span element along the cart icon
+      //  we only want click eventlistener to target cart icon!
+      // console.log(event.target);
+    // event will fire only when clicking on the icon
+      if(event.target.parentElement.classList.contains('store-item-icon')){
+        // you can see its sibling element
+        // console.log(event.target.parentElement);
+        // traverse up the dom to get to the img element
+        // console.log(event.target.parentElement.previousElementSibling.src);
+        let fullPath = event.target.parentElement.previousElementSibling.src;
 
-  })
+        // next i need to parse the string so I can use thumbnail img that matches the bigger img
+        // indexOf() it tells you the position of the img
+        //  I add 3 b/c 'img' is length of 3
+        let pos = fullPath.indexOf('img') + 3;
+        // => pos is index of array img
+        // console.log(pos); 
+        // console.log(fullPath); 
+        // => img/sweets-2.jpeg > img-cart/sweets-2.jpeg
+        //  slice(1: index where we want to start removing, 2:how many to remove? if no second param is given it will take off til the end of the string)
+
+        let partPath = fullPath.slice(pos);
+        // let's view partial path on console.
+        // console.log(partPath);
+        // here try taking out +3 in pos.  notice you are getting the img/!
+        // let pos = fullPath.indexOf('img'); 
+        // => img/doughnut-2.jpeg
+        const item = {};
+        // get the folder where I am getting img from
+        item.img = `img-cart${partPath}`;
+
+        // console.log(item); // => {img: "img-cart/cupcake-3.jpeg"}
+
+      }
+    });
+  });
 })();
