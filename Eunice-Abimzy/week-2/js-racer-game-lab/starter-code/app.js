b1262df08adbb125df307b25a996539275e2f79e
Abimzy <eabiemo@gmail.com>
2019-02-03 23:05:17 -0800

    game logic updated

--- Eunice-Abimzy/week-2/js-racer-game-lab/starter-code/app.js
+++ Eunice-Abimzy/week-2/js-racer-game-lab/starter-code/app.js
@@ -1,9 +1,9 @@
 //Game only moves with click of a button
 //To-do:
-//Use the keyword
+//Use the keyCode
 
 //Use global variables to keep the score and increment on each click
-//find element by classnames
+//find element by classNames
 
 //Use keycode for selecting click functions
 //Left key for clicking= tab(keycode-9)
@@ -13,14 +13,26 @@
 //Element selectors for players:
  var player1 = document.querySelector(".player1");
  var player2 = document.querySelector(".player2");
+ var mover1 = 3;
+ var mover2 = 3;
+
+ //start button to begin game
+ // var button = document.querySelector("button");
 
  //Adding event listeners
- player1.addEventListener("click", movePlayer);
- function movePlayer (){
-  if (e.keyCode === 9){
-    margin-left
-  }
+ player1.addEventListener("keydown", movePlayer);
+ player2.addEventListener("keydown", movePlayer);
 
+ function movePlayer (e){
+  if (e.keyCode === 9){
+    mover1++;
+    player1.style.marginLeft= `${mover}em`;
+  } else if (e.keyCode === 16){
+    mover2++;
+    player2.style.marginLeft = `${mover}em`
+  }
+console.log("we're are racing");
  }
+ movePlayer(16);
 
 
