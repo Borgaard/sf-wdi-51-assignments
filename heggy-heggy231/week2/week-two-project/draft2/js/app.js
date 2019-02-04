02c81d01f68c6944162bc7e5ee38debd96702e73
Heggyhere <heggyy@gmail.com>
2019-02-04 07:40:27 -0800

    item is showing up on cart

--- heggy-heggy231/week2/week-two-project/draft2/js/app.js
+++ heggy-heggy231/week2/week-two-project/draft2/js/app.js
@@ -56,9 +56,69 @@
         const item = {};
         // get the folder where I am getting img from
         item.img = `img-cart${partPath}`;
+        // let name = event.target.parentElement.parentElement.nextElementSibling;
+        let name = event.target.parentElement.parentElement.nextElementSibling.children[0].children[0].textContent;
+        item.name = name;
+        // console.log(name); 
+        let price = event.target.parentElement.parentElement.nextElementSibling.children[0].children[1].textContent;
+        // slice from the space index:1 "$ 15"
+        // .trim() gets rid of any extra white spaces
+        let finalPrice = price.slice(1).trim();
+        // console.log(price); 
+        // => we are getting $ 10 the price of the item but we don't want $ nor space
+        item.price = finalPrice;
+        console.log(finalPrice); 
+        // => final price is getting us only the number
+        console.log(item); 
+        // => {img: "img-cart/cupcake-3.jpeg"}
+        const cartItem = document.createElement('div');
+        // add below class property to cartItem
+        cartItem.classList.add(
+          'cart-item', 
+          'd-flex', 
+          'justify-content-between', 'text-capitalize', 
+          'my-3');
 
-        // console.log(item); // => {img: "img-cart/cupcake-3.jpeg"}
+        // we insert img-cart/doughnut-2.jpeg from item.img for src of img <img src="${item.img}"
+        // inside p we inject "cart item"
+        // cart item price 10.99 is getting injected
+        cartItem.innerHTML = `
+          <img src="${item.img}" class="img-fluid rounded-circle" id="item-img" alt="">
+          <div class="cart-item-text">
+            
+            <p id="cart-item-title" class="font-weight-bold mb-0">${item.name}</p>
+            <span>$</span>
+            <span id="cart-item-price" class="cart-item-price" class="mb-0">${item.price}</span>
+          </div>
+          <a href="#" id='cart-item-remove' class="cart-item-remove">
+            <i class="fas fa-trash"></i>
+          </a>
+        </div>
+        `;
 
+        // select the cart
+        const cart = document.getElementById('cart');
+        const total = document.querySelector('.cart-total-container');
+        // first we are selecting the cart, then we use method insertBefore (1. what item we like to insert? cartItem)
+        cart.insertBefore(cartItem, total);
+        alert('item added to the cart');
+        showTotals();
+/*
+        <!-- cart item -->
+        <div class="cart-item d-flex justify-content-between text-capitalize my-3">
+          <img src="img-cart/doughnut-2.jpeg" class="img-fluid rounded-circle" id="item-img" alt="">
+          <div class="cart-item-text">
+
+            <p id="cart-item-title" class="font-weight-bold mb-0">cart item</p>
+            <span>$</span>
+            <span id="cart-item-price" class="cart-item-price" class="mb-0">10.99</span>
+          </div>
+          <a href="#" id='cart-item-remove' class="cart-item-remove">
+            <i class="fas fa-trash"></i>
+          </a>
+        </div>
+        <!--end of  cart item -->
+*/
       }
     });
   });
