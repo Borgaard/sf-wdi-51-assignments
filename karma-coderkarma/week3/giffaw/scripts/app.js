65f51cccccda6ccf592fb56c2ad32e073c8b20f3
karma <karmadrukpa11@gmail.com>
2019-02-05 19:42:11 -0800

    Just finished api call

---
+++ karma-coderkarma/week3/giffaw/scripts/app.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    let gifyImages = "http://api.giphy.com/v1/gifs/search?q="
+    let key = "&api_key=c82diwDEqDrm7rTiFQpXKdpBVKBb0cXH";
+
+    $("form").on("submit", (e) => {
+        e.preventDefault();
+        alert("event firing");
+        let $gifgallary = $(".gif-gallery");
+
+
+        let gify = $(".form-control.gif-input").val();
+
+        $.ajax({
+            method: "GET",
+            url: gifyImages + gify + key,
+            success: (response) => {
+                console.log(response.data);
+                response.data.map(ele => {
+                    let newGif = `<img src="${ele.images.downsized.url }">`;
+                    $gifgallary.append(newGif)
+                })
+
+            },
+            error: (error) => {
+                console.log(error)
+            }
+
+        })
+    })
+});
