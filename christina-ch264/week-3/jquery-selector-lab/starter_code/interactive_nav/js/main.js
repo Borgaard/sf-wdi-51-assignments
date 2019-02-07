ea086984c6c82e57e7225215eee819986051a975
ch264 <christinavhastenrath@gmail.com>
2019-02-06 22:09:40 -0800

    hw 02/06 and update shopping cart and nav bar

--- christina-ch264/week-3/jquery-selector-lab/starter_code/interactive_nav/js/main.js
+++ christina-ch264/week-3/jquery-selector-lab/starter_code/interactive_nav/js/main.js
@@ -14,46 +14,32 @@
 
 // [x] When the user clicks on an anchor in the main nav:
 
-
+// even if nothing is displayed, first remove the subnav and then show it again
 // If the subnav is already showing and the user clicks on a different anchor than they had previously clicked on, hide the current panel and fade in the related panel with subnav options for that category.
 // If the subnav is showing and the user clicks on the category that is already showing, slide up the subnav.
 $(document).ready(function(){
-  let subnav = $('#subnav');
-  console.log(subnav);
+
   // When the user clicks on an anchor in the main nav:
   $('#main-nav a').on('click', function(){
-    // declare the data related sub nav
-    let subSection = $(this).data('related-panel');
 
-    // add the active class to clicked nav a
-    $(this).addClass('active');
+        // declare the data related sub nav
+        let subSection = $(this).data('related-panel');
 
-      // if nav a has been clicked, show subnav
-      if ($(this).hasClass('active')) {
-
-        // Check if any subnav elements have display: block style attr
-        if ($('#subnav').children().css('display') === 'block') {
-          // what is the jquery method for getting children
-          // $(.subnav).children();
-          // what is the jquery method for checking style attributes
-        // // If yes, use .hide()
-            $('#' + subSection).hide();
-          }
-            $('#' + subSection).show();
-          }
-      // remove class active from nav a when other nav a is clicked
-      $('#main-nav a').removeClass('active');
-      // add class active to the currently clicked nav a
-      $(this).addClass('active');
-
-      // now you should hide the other subSections. STUCK
-      // $('#' + subSection).hide();
-
-    console.log("works")
-
-    });
+      // first hide the subnav
+      if ($(this).children().css('display') === 'block') {
+          $(this).removeClass('active');
+      } else {
+          $('#main-nav a').removeClass('active');
+          // add the active class to clicked nav a
+          $(this).addClass('active');
+          // hide the nav panel
+          $('.nav-panel').hide();
+      }
+      // show the nav panel
+        $(`#${subSection}`).show();
+  });
 });
 
-      // $('#women').css('display', "block");
+
 
 
