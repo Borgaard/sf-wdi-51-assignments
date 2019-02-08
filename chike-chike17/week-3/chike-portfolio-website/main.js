2ede2c1cf51afc5c5fa0416f3d2efde8c9be5d66
CO17 <chike.onuorah@gmail.com>
2019-02-08 15:38:50 -0800

    organize content, implement auto-scroll

---
+++ chike-chike17/week-3/chike-portfolio-website/main.js
@@ -0,0 +1,10 @@
+
+
+console.log($)
+$("nav").find("a").click(function(e) {
+    e.preventDefault();
+    var section = $(this).attr("href");
+    $("html, body").animate({
+        scrollTop: $(section).offset().top
+    }, 1500);
+});
