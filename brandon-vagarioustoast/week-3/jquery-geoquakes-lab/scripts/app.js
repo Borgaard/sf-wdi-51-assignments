76f04dca8efc1530c6d91bcbb54783e1b1a71e51
vagarioustoast <vagarioustoast@gmail.com>
2019-02-07 16:36:54 -0800

    initial project commit

--- brandon-vagarioustoast/week-3/jquery-geoquakes-lab/scripts/app.js
+++ brandon-vagarioustoast/week-3/jquery-geoquakes-lab/scripts/app.js
@@ -1,50 +1,62 @@
 // define globals
-const weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
+const weekly_quakes_endpoint =
+  "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
 
-$(()=> {
+$(() => {
   console.log("Let's get coding!");
   // CODE IN HERE!
-  const apiKey = 'AIzaSyBHLett8djBo62dDXj0EjCimF8Rd6E8cxg';
+  const apiKey = "AIzaSyBHLett8djBo62dDXj0EjCimF8Rd6E8cxg";
   const icon = {
-        url: "images/earthquake.png", // url
-        scaledSize: new google.maps.Size(30, 30), // size
-    };
+    url: "images/earthquake.png", // url
+    scaledSize: new google.maps.Size(30, 30) // size
+  };
 
   // Initial Ajax Call
   $.ajax({
-    method: 'GET',
+    method: "GET",
     url: weekly_quakes_endpoint,
-    success: (res) =>{
-        let map;
-        function initMap() {
-          map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 5,
-            center: {lat: res.features[0].geometry.coordinates[1], lng: res.features[0].geometry.coordinates[0]}
-          });
-        };
+    success: res => {
+      let map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById("map"), {
+          zoom: 5,
+          center: {
+            lat: res.features[0].geometry.coordinates[1],
+            lng: res.features[0].geometry.coordinates[0]
+          }
+        });
+      }
 
-         initMap();
+      initMap();
 
-        let marker = new google.maps.Marker({
-            position: {lat: res.features[0].geometry.coordinates[1], lng: res.features[0].geometry.coordinates[0]},
-            map: map,
-            title: 'Hello World!',
-            icon: icon
-          });
+      let marker = new google.maps.Marker({
+        position: {
+          lat: res.features[0].geometry.coordinates[1],
+          lng: res.features[0].geometry.coordinates[0]
+        },
+        map: map,
+        title: "Hello World!",
+        icon: icon
+      });
 
-        res.features.map(index => {
+      res.features.map(index => {
         // Appends To New Quake Data To DOM
-        let newQuake = `<p>${index.properties.title}, ${index.properties.time}</p>`
-        $('#info').append(newQuake);
+        let newQuake = `<p>${index.properties.title}, ${
+          index.properties.time
+        }</p>`;
+        $("#info").append(newQuake);
 
         // Adds Map and Markers
 
-        let newLocation = {lat: index.geometry.coordinates[1], lng: index.geometry.coordinates[0]};
-          console.log(newLocation);
+        let newLocation = {
+          lat: index.geometry.coordinates[1],
+          lng: index.geometry.coordinates[0]
+        };
+        console.log(newLocation);
 
-          marker.setMap(map);
-      })
+        marker.setMap(map);
+      });
     },
     error: error => console.log(error)
-  })
+  });
 });
