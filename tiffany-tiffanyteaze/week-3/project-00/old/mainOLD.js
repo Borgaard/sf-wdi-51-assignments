a7b8615884112c78c8d685d195cde922ce1c54bb
tiffanyteaze <tteaze@gmail.com>
2019-02-08 12:28:08 -0800

    removing submodules

---
+++ tiffany-tiffanyteaze/week-3/project-00/old/mainOLD.js
@@ -0,0 +1,18 @@
+// const track = document.querySelector('.carousel-track-container');
+// const slides = Array.from(track.children);
+// const nextButton = document.querySelector('.button-left');
+// const prevButton = document.querySelector('.button-right');
+// const dotsNav = document.querySelector('.carousel-nav');
+// const dots = Array.from(dotsNav.children);
+
+// const slideSize = slides[0].getBoundingClientRect();
+// const slideWidth = slideSize.width;
+
+// //arrange the slides next to one another
+// slides[0].style.left = slideWidth * 0 + 'px';
+// slides[1].style.left = slideWidth * 1 + 'px';
+// slides[2].style.left = slideWidth * 2 + 'px';
+
+var bootstrap_enabled = (typeof $().emulateTransitionEnd == 'function');
+
+
