b11d5effd2851160eaef57dd6a0b110c9f256886
alom310 <alom310@gmail.com>
2019-02-08 09:04:06 -0800

    project00 ver1.1

---
+++ alomgir-alom310/week-3/project-00/js/app.js
@@ -0,0 +1,17 @@
+/////////Scroll Functin///////////
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('#scroll').fadeIn();
+        } else {
+            $('#scroll').fadeOut();
+        }
+    });
+    $('#scroll').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 0);
+        return false;
+    });
+});
+
+
+
