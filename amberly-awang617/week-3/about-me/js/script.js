7dd56a43192ff657ee2f13ca3ae63607f04e0af5
awang617 <wanga18@mail.wlu.edu>
2019-02-08 09:05:03 -0800

    project 0 after one day

--- amberly-awang617/week-3/about-me/js/script.js
+++ amberly-awang617/week-3/about-me/js/script.js
@@ -1,6 +1,26 @@
-function clearForm(e) {
-  e.preventDefault();
-	document.getElementById('myForm').reset();
+// Make sure jquery is working
+// $( document ).ready(function() {
+//     console.log( "ready!" );
+// });
+var testimony;
+
+let fadeAway = function() {
+  $('.testimony').fadeOut("slow");
 }
 
+let fadeBack = function() {
+  $('.testimony').empty();
+  $('.testimony').append(`<p>Hello there</p>`);
+  $('.testimony').fadeIn("slow");
+}
+
+setInterval(fadeAway, 10000);
+setInterval(fadeBack, 10050);
+
 document.querySelector("button").addEventListener('click', clearForm)
+
+function clearForm(e) {
+  e.preventDefault();
+  document.getElementById('myForm').reset();
+}
+
