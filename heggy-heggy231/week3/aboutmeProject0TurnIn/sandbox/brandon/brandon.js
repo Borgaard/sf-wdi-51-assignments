566f0ada5620204c87fa12712993aa1805c9257f
Heggyhere <heggyy@gmail.com>
2019-02-09 23:58:44 -0800

    implemented forloop limited

--- heggy-heggy231/week3/aboutmeProject0TurnIn/sandbox/brandon/brandon.js
+++ heggy-heggy231/week3/aboutmeProject0TurnIn/sandbox/brandon/brandon.js
@@ -1,3 +1,11 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+  console.log( "ready!" );
+
+
+});
+/*
+brandon's code
 var texts = [$('.example'), "example2", "example3"];
 var count = 0;
 function changeText() {
@@ -5,3 +13,18 @@ function changeText() {
     count < 3 ? count++ : count = 0;
 }
 setInterval(changeText, 10000);
+*/
+
+/* 
+Heggy's code
+var texts = ['example1', 'example2', 'example3'];
+
+function changeText() {
+    for(var i = 0; i < texts.length; i++){
+        $("#example").text(texts[i]);
+    }
+}
+
+debugger;
+setInterval(changeText, 2000);
+*/ 
