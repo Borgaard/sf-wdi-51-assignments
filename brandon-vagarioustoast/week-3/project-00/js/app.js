6744d11412dbbef98d54f41bc30ca13c8fc2659c
vagarioustoast <vagarioustoast@gmail.com>
2019-02-09 20:38:34 -0800

    added structure

--- brandon-vagarioustoast/week-3/project-00/js/app.js
+++ brandon-vagarioustoast/week-3/project-00/js/app.js
@@ -1,1 +1,9 @@
 console.log(`We're online.`);
+
+document.addEventListener('DOMContentLoaded', () => {
+    let elems = document.querySelectorAll('.materialboxed');
+    let instances = M.Materialbox.init(elems);
+
+
+
+})
