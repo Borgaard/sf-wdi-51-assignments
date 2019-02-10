dc7d9ed90feb9558ab02976b3cd0ffdb72b2b738
vagarioustoast <vagarioustoast@gmail.com>
2019-02-09 21:13:40 -0800

    added first image

--- brandon-vagarioustoast/week-3/project-00/js/app.js
+++ brandon-vagarioustoast/week-3/project-00/js/app.js
@@ -4,6 +4,9 @@ document.addEventListener('DOMContentLoaded', () => {
     let elems = document.querySelectorAll('.materialboxed');
     let instances = M.Materialbox.init(elems);
 
+    // var elemsCarousel = document.querySelectorAll('.carousel');
+    // var instancesCarousel = M.Carousel.init(elemsCarousel);
+
 
 
 })
