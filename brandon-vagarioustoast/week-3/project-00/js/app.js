a725b14b2acbcac605155d5792c8aa2e983106e9
vagarioustoast <vagarioustoast@gmail.com>
2019-02-10 19:14:59 -0800

    added profile and more responsivity

--- brandon-vagarioustoast/week-3/project-00/js/app.js
+++ brandon-vagarioustoast/week-3/project-00/js/app.js
@@ -1,11 +1,27 @@
 console.log(`We're online.`);
 
 document.addEventListener('DOMContentLoaded', () => {
+    // Materialize Methods
+
     let elems = document.querySelectorAll('.materialboxed');
     let carouselImages = document.querySelectorAll('.carousel');
+    let sideNavElem = document.querySelectorAll('.sidenav');
 
     let instances = M.Materialbox.init(elems);
     let carouselInstance = M.Carousel.init(carouselImages, {
       autoplay: true,
+    });
+    let sideNavInstance = M.Sidenav.init(sideNavElem);
+
+
+
+    // Disable Submit For Now
+    let submitButton = document.querySelector('.submit-button');
+    submitButton.addEventListener('click', (e)=>{
+      e.preventDefault();
     })
+
+
+
+
   })
