c27e7b328352e65c63bb97a6df623beab43f3f68
BryantBuilt <bryantp24@gmail.com>
2019-02-10 19:39:40 -0800

    attempting to fix dropdown on mobile

--- bryant-bryantbuilt/week-3/project-00/js/main.js
+++ bryant-bryantbuilt/week-3/project-00/js/main.js
@@ -1,9 +1,20 @@
+console.log("Main Connected");
+
 $('document').ready(function(){
+  console.log("jQuery Connected");
 
+  $('.dropdown-trigger').dropdown();
+//   $('.dropdown-content li a').on('click', function() {
+//     window.location.href = $(this).attr('href');
+// }
 
+  $('.carousel').carousel();
+  // let instance = M.Carousel.getInstance(elem);
 
-
-
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
 
 
 });
