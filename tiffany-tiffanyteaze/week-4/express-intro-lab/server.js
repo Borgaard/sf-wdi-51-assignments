896fff9c4102d10506fe3565b7048385e6b221f3
tiffanyteaze <tteaze@gmail.com>
2019-02-12 09:15:18 -0800

    Added README to Project 00

---
+++ tiffany-tiffanyteaze/week-4/express-intro-lab/server.js
@@ -0,0 +1,15 @@
+// server.js
+  // SERVER-SIDE JAVASCRIPT
+  const express = require('express');
+  const app = express();
+
+  // Allow CORS: we'll use this today to reduce security so we can more easily test our code in the browser.
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+  app.get('/', (req, res) => res.send('Hello World!'));
+
+  app.listen(process.env.PORT || 3000,  () => console.log('Example app listening at http://localhost:3000/'));
