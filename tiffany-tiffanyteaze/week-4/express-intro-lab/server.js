3a4c4359f38391a6762cb361bfc8e7dbe3565ab4
tiffanyteaze <tteaze@gmail.com>
2019-02-13 09:06:26 -0800

    Started Express Intro Lab but getting app.js 404

--- tiffany-tiffanyteaze/week-4/express-intro-lab/server.js
+++ tiffany-tiffanyteaze/week-4/express-intro-lab/server.js
@@ -3,6 +3,31 @@
   const express = require('express');
   const app = express();
 
+//array of albums
+const albums = [
+  {
+    title: 'Cupid Deluxe',
+    artist: 'Blood Orange'
+  },
+  {
+    title: 'M3LL155X - EP',
+    artist: 'FKA twigs'
+  },
+  {
+    title: 'Fake History',
+    artist: 'letlive.'
+  }
+];
+
+//array of taquerias
+const taquerias = [
+  { name: "La Taqueria" },
+  { name: "El Farolito" },
+  { name: "Taqueria Cancun" }
+];
+
+app.use(express.static('public'));
+
   // Allow CORS: we'll use this today to reduce security so we can more easily test our code in the browser.
   app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
@@ -10,6 +35,11 @@
     next();
   });
 
-  app.get('/', (req, res) => res.send('Hello World!'));
+  app.get('/', (req, res) => res.sendFile('views/index.html' , {root : __dirname}));
+
+  app.get('/api/albums', (req, res) => res.json(albums));
+
+  app.get('/api/taquerias', (req, res) => res.json(taquerias));
+
 
   app.listen(process.env.PORT || 3000,  () => console.log('Example app listening at http://localhost:3000/'));
