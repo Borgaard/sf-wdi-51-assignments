01ccfdfb51c6c6d45b57db392b9c2327ea16feae
vagarioustoast <vagarioustoast@gmail.com>
2019-02-13 11:19:45 -0800

    sets up model for books and connects to server

---
+++ brandon-vagarioustoast/week-4/mongoose-book-app/models/book.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema({
+  title: String,
+  author: String,
+  image: String,
+  date: String
+});
+
+const Book = mongoose.model("Book", BookSchema);
+
+module.exports = Book;
