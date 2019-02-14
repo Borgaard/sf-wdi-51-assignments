ee1f143881b2d29a7f42db66859b60db73fe489c
alom310 <alom310@gmail.com>
2019-02-14 12:18:02 -0800

    moved express personal lab to another folder

--- alomgir-alom310/week-4/express-personal-api/models/index.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require("mongoose");
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/personal-api", {useMongoClient: true});
-
-// module.exports.Campsite = require("./campsite.js.example");
