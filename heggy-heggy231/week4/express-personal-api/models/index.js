30de7fa28570bce49535323f2a4ca9696140ce7d
Heggyhere <heggyy@gmail.com>
2019-02-14 12:31:54 -0800

    heroku know what port to run on, and what database to connect to

--- heggy-heggy231/week4/express-personal-api/models/index.js
+++ heggy-heggy231/week4/express-personal-api/models/index.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+// Update your database connection to point to Heroku's database. Open models/index.js and add the following to the mongoose.connect method
+// what port to run on, and what database to connect to
 mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/personal-api", {useMongoClient: true});
 
 // module.exports.Campsite = require("./campsite.js.example");
