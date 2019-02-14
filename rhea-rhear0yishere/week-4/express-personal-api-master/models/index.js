594fd6e1bb7a22e2d9f69a3cb36c6e782db1b6e2
Rhea Roy <rroy.rhea@gmail.com>
2019-02-14 12:23:38 -0800

    heroku addon monglab installed

--- rhea-rhear0yishere/week-4/express-personal-api-master/models/index.js
+++ rhea-rhear0yishere/week-4/express-personal-api-master/models/index.js
@@ -2,3 +2,4 @@ const mongoose = require("mongoose");
 mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/personal-api", {useMongoClient: true});
 
 // module.exports.Campsite = require("./campsite.js.example");
+
