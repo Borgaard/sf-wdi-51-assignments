d3ebef9c2930bcc13c4c675d8640a8db3839f2e6
ch264 <christinavhastenrath@gmail.com>
2019-02-14 21:38:57 -0800

    safety save

--- christina-ch264/week-4/express-personal-api/models/index.js
+++ christina-ch264/week-4/express-personal-api/models/index.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-// mongoose.connect( process.env.MONGODB_URI || "YOUR CURRENT LOCALHOST DB CONNECTION STRING HERE" );
 mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/personal-api", {useMongoClient: true});
 
 // module.exports.Campsite = require("./campsite.js.example");
--- christina-ch264/week-4/express-personal-api/server.js
+++ christina-ch264/week-4/express-personal-api/server.js
@@ -47,17 +47,40 @@ app.get('/api', (req, res) => {
   // It would be seriously overkill to save any of this to your database.
   // But you should change almost every line of this response.
   res.json({
-    woopsIForgotToDocumentAllMyEndpoints: true, // CHANGE ME ;)
+    ChristinaEndpoints: true,
     message: "Welcome to my personal api! Here's what you need to know!",
-    documentationUrl: "https://github.com/example-username/express-personal-api/README.md", // CHANGE ME
-    baseUrl: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
+    documentationUrl: "https://github.com/ch264/week4/express-personal-api/README.md", //Change to my personal github readme file
+    baseUrl: "https://agile-island-65053.herokuapp.com", // Changed to my heroku url
     endpoints: [
-      {method: "GET", path: "/api", description: "Describes all available endpoints"},
-      {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
-      {method: "POST", path: "/api/campsites", description: "E.g. Create a new campsite"} // CHANGE ME
+      {
+        method: "GET", 
+        path: "/api", 
+        description: "Describes all available endpoints"
+      },
+      {
+        method: "GET", 
+        path: "/api/profile", 
+        description: "Data about me"
+      }, // CHANGE ME
+      {
+        method: "DELETE", 
+        path: "/api/campsites", 
+        description: "E.g. Create a new campsite"
+      } // CHANGE ME
+      {
+        method: "PUT", 
+        path: "/api/campsites", 
+        description: "E.g. Create a new campsite"
+      } // CHANGE ME
     ]
   })
 });
+
+app.get('/api/profile',(req, res) => {
+  res.json({
+  social: 'Linkedin'
+  })
+})
 
 /**********
  * SERVER *
