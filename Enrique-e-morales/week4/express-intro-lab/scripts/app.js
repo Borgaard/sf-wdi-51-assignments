ae1fd276b28b425a08d03a68431a0d3eb5ccf60a
e-morales <e.morales@digitalnest.org>
2019-02-20 13:59:50 -0800

    commiting after removing git

---
+++ Enrique-e-morales/week4/express-intro-lab/scripts/app.js
@@ -0,0 +1,20 @@
+console.log("Sanity Check: JS is working!");
+
+const handleSuccess = json =>  console.log(json);
+
+
+$(document).ready(() => {
+
+
+  const handleError = (xhr, status, errorThrown) => console.log('uh oh');
+
+
+  $.ajax({
+    method: 'GET',
+    url: 'http://localhost:3000/api/albums',
+    success: handleSuccess,
+    error: handleError
+  });
+
+});
+
