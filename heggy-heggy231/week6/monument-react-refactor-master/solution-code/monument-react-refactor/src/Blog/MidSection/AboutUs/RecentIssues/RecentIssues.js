c0696f35512786b792fa9f58e191004db8eb5c69
Heggyhere <heggyy@gmail.com>
2019-02-25 14:11:07 -0800

    start react Monument

---
+++ heggy-heggy231/week6/monument-react-refactor-master/solution-code/monument-react-refactor/src/Blog/MidSection/AboutUs/RecentIssues/RecentIssues.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const recentissues = () => {
+    return (
+        <div>
+            <h3>Recent Issues</h3>
+                <ul>
+                    <li><a href="">Issue Nineteen &mdash; Camping</a></li>
+                    <li><a href="">Issue Eighteen &mdash; Food</a></li>
+                    <li><a href="">Issue Seventeen &mdash; Signs</a></li>
+                    <li><a href="">Issue Sixteen &mdash; Friends</a></li>
+                    <li><a href="">Issue Fifteen &mdash; Carnivals</a></li>
+                    <li><a href="">Issue Fourteen &mdash; Gatherings</a></li>
+                </ul>
+        </div>
+    )
+}
+
+export default recentissues;
