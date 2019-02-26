d9f2eca68628eca922cd85b7446e7872ff8efba9
Abimzy <eabiemo@gmail.com>
2019-02-25 16:13:33 -0800

    Created HeroHeader component

---
+++ Eunice-Abimzy/week-6/monument/monument-app/src/HeroHeader.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class HeroHeader extends Component {
+    render (){
+        return (
+            <header className="vertical-center">
+
+		<div>
+			<h1><a href="">Monument</a></h1>
+			<h2>A Lifestyle Magazine</h2>
+		</div>
+	</header>
+        );
+    }
+}
+export default HeroHeader
