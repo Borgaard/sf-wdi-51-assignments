b00c222e2d8ee3592ae9ba4c73c3e4c552528f07
awang617 <wanga18@mail.wlu.edu>
2019-02-25 22:41:09 -0800

    changed name from Header to BlogHeader

---
+++ amberly-awang617/week-6/monument-app/src/Blog/BlogHeader/BlogHeader.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './blogHeader.css'
+
+class BlogHeader extends Component {
+    render() {
+        return (
+            <header class="vertical-center">
+                <div>
+                    <h1><a href="index.html">Monument</a></h1>
+                </div>
+            </header>
+        )
+    }
+}
+
+export default BlogHeader;
