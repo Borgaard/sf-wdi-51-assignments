debf39f527187430c7535413d69241a7f9ce09fc
awang617 <wanga18@mail.wlu.edu>
2019-02-25 22:42:11 -0800

    added Blog component, commented out

--- amberly-awang617/week-6/monument-app/src/App.js
+++ amberly-awang617/week-6/monument-app/src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import NavBar from './Index/Navbar/Navbar'
 import Index from './Index/Index'
 import Footer from './Index/Footer/Footer'
+// import Blog from './Blog/Blog'
 
 class App extends Component {
   render() {
     return (
       <div>
       <NavBar />
-
+      {/* <Blog /> */}
       <Index />
 
       <Footer />
