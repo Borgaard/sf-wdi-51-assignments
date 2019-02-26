fcbe02d4059901cdb7545d2c63f62c4c8569c4ab
awang617 <wanga18@mail.wlu.edu>
2019-02-25 16:38:27 -0800

    gallery css and images

--- amberly-awang617/week-6/monument-app/src/Index/Gallery/Gallery.js
+++ amberly-awang617/week-6/monument-app/src/Index/Gallery/Gallery.js
@@ -1,4 +1,11 @@
 import React, { Component } from 'react';
+import './gallery.css'
+import Gallery1 from './gallery_1.jpg'
+import Gallery2 from './gallery_2.jpg'
+import Gallery3 from './gallery_3.jpg'
+import Gallery4 from './gallery_4.jpg'
+import Gallery5 from './gallery_5.jpg'
+import Gallery6 from './gallery_6.jpg'
 
 class Gallery extends Component {
     render() {
@@ -8,12 +15,12 @@ class Gallery extends Component {
                     <h2>Issue Twenty</h2>
                     <h3>A visual guide to the Southwest</h3>
                     <div className="masonry">
-                        <img src="images/gallery_1.jpg" alt="" />
-                        <img src="images/gallery_3.jpg" alt="" />
-                        <img src="images/gallery_4.jpg" alt="" />
-                        <img src="images/gallery_2.jpg" alt="" />
-                        <img src="images/gallery_5.jpg" alt="" />
-                        <img src="images/gallery_6.jpg" alt="" />
+                        <img src={Gallery1} alt="" />
+                        <img src={Gallery3} alt="" />
+                        <img src={Gallery4} alt="" />
+                        <img src={Gallery2} alt="" />
+                        <img src={Gallery5} alt="" />
+                        <img src={Gallery6} alt="" />
                     </div>
                     </div>
                 </section> 
