9c8f1b533fd272855eab2c5c173862d1f01af2b6
Heggyhere <heggyy@gmail.com>
2019-02-25 18:38:34 -0800

    created Contact Component, Blog.js now imports Contact.js

--- heggy-heggy231/week6/monument-react-refactor-master/heggy-monument-react-app/src/Blog/Blog.js
+++ heggy-heggy231/week6/monument-react-refactor-master/heggy-monument-react-app/src/Blog/Blog.js
@@ -1,11 +1,16 @@
 // import Compenent here! since I am creating component
 import React, { Component } from 'react';
 
+
+import Contact from './Contact/Contact';
+
 class Blog extends Component {
   render () {
     return (
       <div>
 
+
+        <Contact />
       </div>
 
     )
