0ce241b31bdd3652a56cd7288f04e6e58a48375d
BryantBuilt <bryantp24@gmail.com>
2019-02-26 09:14:32 -0800

    Old week-4 homework

---
+++ bryant-bryantbuilt/week-4/express-personal-api/models/movie.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose'),
+Schema = mongoose.Schema;
+
+const movieSchema = new Schema({
+    name: String,
+    releaseDate: Date,
+    rating: Number,
+    viewDate: Date
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+
+module.exports = Movie;
+
