43c3f5688fe15810cf652e3f653fd6ec879772ca
Heggyhere <heggyy@gmail.com>
2019-02-25 18:29:57 -0800

    created Blog.js Component set up

---
+++ heggy-heggy231/week6/monument-react-refactor-master/heggy-monument-react-app/src/Blog/Blog.js
@@ -0,0 +1,15 @@
+// import Compenent here! since I am creating component
+import React, { Component } from 'react';
+
+class Blog extends Component {
+  render () {
+    return (
+      <div>
+
+      </div>
+
+    )
+  }
+}
+
+export default Blog;
