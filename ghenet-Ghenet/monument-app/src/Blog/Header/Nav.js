968e752af26266a6a24626fbed31f9fca5e9c4e5
Ghenet Kibrom <ghenetkibrom@Ghenets-MacBook-Pro.local>
2019-02-26 06:13:54 -0800

    Hw Monument refactor react app submission

---
+++ ghenet-Ghenet/monument-app/src/Blog/Header/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Nav.css';
+
+const nav = ()=>{
+    return (
+        <div>
+        <header className="vertical-center1">
+            <div>
+                <h1><a href="http://localhost:3000/">Monument1</a></h1>
+            </div>
+        </header>
+    </div> 
+    )
+}
+
+export default nav;
