4ad33fc5f466f547b983dd748532de59636080b6
Abimzy <eabiemo@gmail.com>
2019-02-26 19:32:37 -0800

    Created Counter component with placeholder 'counter' variable for UI

--- Eunice-Abimzy/week-6/react-counters/src/Counter.js
+++ Eunice-Abimzy/week-6/react-counters/src/Counter.js
@@ -1,35 +1,16 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-class Counter extends Component {
-  // need to have counter value (0,1,2,-3)
-
-  state = {
-    count: 0
-  }
-
-  increment = () => {
-    let newCount = this.state.count + 1
-    this.setState({
-      count: newCount
-    })
-  }
-  
-  decrement = () => {
-    let newCount = this.state.count - 1
-    this.setState({
-      count: newCount
-    })  
-  }
+ class Counter extends Component {
   render() {
-
+    // counter value to be done dynamically later
+    let counter = ` -5`; 
     return (
-      <div className='Counter' >
-        <h4>Counter: {this.state.count}</h4>
-        <button onClick={this.increment}>Increment</button>
-        <button onClick={this.decrement}>Decrement</button>
+      <div>
+        <h2>Counter:{counter}</h2>
+        <button>Decrement</button> <button>Increment</button>
       </div>
-    );
+    )
   }
 }
+export default Counter
 
-export default Counter;
