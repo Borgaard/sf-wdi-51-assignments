38ad42943051c02b429dccd5718f9f1899f6f006
Heggyhere <heggyy@gmail.com>
2019-02-26 19:48:28 -0800

    define Define an increaseCounters() method and decreaseCounters() method on the App component

--- heggy-heggy231/week6/react-counters-master/src/App.js
+++ heggy-heggy231/week6/react-counters-master/src/App.js
@@ -14,8 +14,25 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
+        {/* Define an increaseCounters() method and decreaseCounters() method on the App component (they'll be a lot like the increaseCount() and decreaseCount()methods of ourCounter` component).
+         */}
         {/* index.js data.counter =5 is coming here */}
         {/* <h4>{this.props.data.counters}</h4> */}
+        increaseCount = () => {
+          let count = this.state.count + 1;
+
+          this.setState({
+            count
+          });
+        }
+
+        decreaseCount = () => {
+          let count = this.state.count - 1;
+
+          this.setState({
+            count
+          });
+        }
         <CounterList counter={this.state.counters} />
         {/* <CounterList counters={this.props.data.counters}/> */}
       </div>
