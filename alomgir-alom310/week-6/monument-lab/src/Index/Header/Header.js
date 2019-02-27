c83205d27a547a42dfaeacb568b604be9573d9b8
alom310 <alom310@gmail.com>
2019-02-26 20:48:10 -0800

    homework react monument lab

---
+++ alomgir-alom310/week-6/monument-lab/src/Index/Header/Header.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+import './Header.css'
+
+class Header extends Component {
+    render(){
+        return (
+            <header class="vertical-center">
+                <div>
+                    <h1><a href="">Monument</a></h1>
+                    <h2>A Lifestyle Magazine</h2>
+                </div>
+            </header>
+        )
+    }
+}
+
+export default Header;
