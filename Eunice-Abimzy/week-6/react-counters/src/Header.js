118b5723983c80d6c49adb90ce1f3b7269d2fe58
Abimzy <eabiemo@gmail.com>
2019-02-26 19:16:15 -0800

    Created Header component with increment and decrement counter

--- Eunice-Abimzy/week-6/react-counters/src/Header.js
+++ Eunice-Abimzy/week-6/react-counters/src/Header.js
@@ -1,30 +1,14 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
 class Header extends Component {
-  
-  handleIncrement = () => {
-    console.log("you pressed + !")
-    this.props.increaseCounters()
-  }
-
-  handleDecrement = () => {
-
-    this.props.countersCount > 0 ?
-      this.props.decreaseCounters():
-      null
-  }
-
   render() {
-
-
     return (
-      <header className="App-header">
-        <h1 className="App-title">React Counters</h1>
-        <button onClick={ this.handleIncrement }>+</button>
-        <button onClick={ this.handleDecrement }>-</button>
-      </header>
-    );
+      <div>
+        <h1>React Counters</h1>
+        <button>+</button><button>-</button>
+      </div>
+    )
   }
 }
+export default Header
 
-export default Header;
