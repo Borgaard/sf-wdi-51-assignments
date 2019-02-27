2b5867a894fc80f7d6e7158f4070ee8948676d21
Heggyhere <heggyy@gmail.com>
2019-02-26 19:38:22 -0800

    App component is tracking the number of counters (5) inside state and passing that as a prop to the CounterList component.

--- heggy-heggy231/week6/react-counters-master/src/App.js
+++ heggy-heggy231/week6/react-counters-master/src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from "react";
 import Header from "./Header";
-// import CounterList from "./CounterList";
+import CounterList from "./CounterList";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      counters: 5
+    };
+  }
   render() {
     return (
       <div className="App">
         <Header />
         {/* index.js data.counter =5 is coming here */}
-        <h4>{this.props.data.counters}</h4>
+        {/* <h4>{this.props.data.counters}</h4> */}
+        <CounterList counter={this.state.counters} />
         {/* <CounterList counters={this.props.data.counters}/> */}
       </div>
     );
