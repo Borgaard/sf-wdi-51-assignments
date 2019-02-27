7f1d83d937e9ea88a6924b175c96ffc8a46d17f8
Heggyhere <heggyy@gmail.com>
2019-02-26 19:33:11 -0800

    track Counter to track inside of state counter.js

--- heggy-heggy231/week6/react-counters-master/src/Counter.js
+++ heggy-heggy231/week6/react-counters-master/src/Counter.js
@@ -1,17 +1,42 @@
+// Counter component so that it is using state to track the count internally and clicking one of the buttons changes the count number.
+
 import React, { Component } from "react";
 
 // create Counter Component
+// Counter component to keep track of the count in state
 class Counter extends Component {
+  state = {
+    count: 0
+  };
+
   render() {
     return (
       <div className="Counter">
         <h4>Counter: 0</h4>
-        <button>Decrement</button>
-        <button>Increment</button>
+        {/* <button>Decrement</button> */}
+        <button onClick={this.increaseCount}>Increment</button>
+        {/* <button>Increment</button> */}
+        <button onClick={this.decreaseCount}>Decrement</button>
       </div>
     );
   }
+
+  increaseCount = () => {
+    let count = this.state.count + 1;
+
+    this.setState({
+      count
+    });
+  }
+
+  decreaseCount = () => {
+    let count = this.state.count - 1;
+
+    this.setState({
+      count
+    });
+  }
 }
 
-//  make counter avaialble
+//  make counter available
 export default Counter;
--- heggy-heggy231/week6/react-counters-master/src/CounterList.js
+++ heggy-heggy231/week6/react-counters-master/src/CounterList.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import Counter from "./Counter";
-import App from "./App";
+// import App from "./App";
 
 class ContainerList extends Component {
   render() {
     let counters = [];
     // this.props.counters is from index.js data.counters =5  > App.js
-    for( let index = 0; index < this.props.counters; index++) {
+    for( let index = 0; index < this.props.counters; index++ ) {
       // counters = [<Item />, <Item />, <Item />, ....];
       counters.push(<Counter />);
     }
