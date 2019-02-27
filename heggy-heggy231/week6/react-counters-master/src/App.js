ad3ff1646501b039cb66222e5a4ec8fe920f4ce7
Heggyhere <heggyy@gmail.com>
2019-02-26 18:10:16 -0800

    hard code data as static

---
+++ heggy-heggy231/week6/react-counters-master/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Header from "./Header";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        
+      </div>
+    );
+  }
+}
---
+++ heggy-heggy231/week6/react-counters-master/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+// hard code data
+const data = {
+  counters: 5
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
