4813a61d3cde308cc227b648f85ef84f7b9d5c6e
Abimzy <eabiemo@gmail.com>
2019-02-28 00:31:43 -0800

    Created routes for App and Blog sections of monument magazine

--- Eunice-Abimzy/week-6/monument/monument-app/src/NavBar.js
+++ Eunice-Abimzy/week-6/monument/monument-app/src/NavBar.js
@@ -1,17 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {Route, Link, Switch} from 'react-router-dom';
+import Blog from './Blog/Blog';
+import App from './App';
+
 
 class NavBar extends Component {
     render (){
         return (
+        <div>
             <nav>
             <a class="hamburger" href=""><i class="fa fa-bars"></i></a>
             <ul>
+                {/* <li><Link to="/">About</Link></li> */}
                 <li><a href="#about">About</a></li>
                 <li><a href="#gallery">Gallery</a></li>
-                <li><a href="blog.html">Blog</a></li>
+                <li><Link to="/blog.html">Blog</Link></li>
                 <li><a href="#contact">Contact</a></li>
             </ul>
         </nav>
+       <Switch>
+         <Route exact path="/" component={ App } />
+         <Route path="/blog" component={ Blog} />
+       </Switch>
+       </div>
         );
     }
 }
