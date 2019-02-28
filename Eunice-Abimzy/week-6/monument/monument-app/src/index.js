3f1794d177fdd5ecca9eb5423c4b03b2d62accf1
Abimzy <eabiemo@gmail.com>
2019-02-27 22:26:14 -0800

    Changed Monument and Blog components to be rendered on reactDOM

--- Eunice-Abimzy/week-6/monument/monument-app/src/index.js
+++ Eunice-Abimzy/week-6/monument/monument-app/src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Monument from './App';
+// import Blog from './Blog/Blog';
 import * as serviceWorker from './serviceWorker';
 
 
 ReactDOM.render(<Monument />, document.getElementById('root'));
+
+// ReactDOM.render(<Blog />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
