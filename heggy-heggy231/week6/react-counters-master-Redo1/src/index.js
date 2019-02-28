c300154a36bb847cb1969da0318969c8e6956105
Heggyhere <heggyy@gmail.com>
2019-02-27 21:52:45 -0800

    set data obj inside index.js

--- heggy-heggy231/week6/react-counters-master-Redo1/src/index.js
+++ heggy-heggy231/week6/react-counters-master-Redo1/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-
+// hard code the data into app counters is 5
+const data = {
+  counters: 5
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
