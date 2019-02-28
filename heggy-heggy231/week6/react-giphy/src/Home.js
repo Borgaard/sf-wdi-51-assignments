83dae252bf68bd0bfc1810ecc0d4620210b79e3e
Heggyhere <heggyy@gmail.com>
2019-02-28 01:24:19 -0800

    Add UI for Search Search component in the Home component.

--- heggy-heggy231/week6/react-giphy/src/Home.js
+++ heggy-heggy231/week6/react-giphy/src/Home.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import Search from './components/Search';
 
 class Home extends Component {
   render() {
     return (
+// Remember to put all html inside parent wrapper!!
       <main>
         <h1>Hello World! Heggy here</h1>
+        <section>
+          <Search />
+        </section>
       </main>
+
     );
   }
 }
---
+++ heggy-heggy231/week6/react-giphy/src/components/Search.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  render() {
+    return(
+      <div>
+
+      <input type="text" placeholder="search" />
+
+      <input type="submit" value="Submit" />
+      </div>
+    );
+  }
+
+}
+
+export default Search;
