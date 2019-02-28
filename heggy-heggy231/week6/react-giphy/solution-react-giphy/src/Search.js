f90867b1161c832306ec213e6e6e445eff3ceb3f
Heggyhere <heggyy@gmail.com>
2019-02-28 15:12:58 -0800

    Define your Search component's initial state. It should have a query value t

---
+++ heggy-heggy231/week6/react-giphy/solution-react-giphy/src/Search.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  // state goe here
+  state = {
+    query: ''
+  }
+  render() {
+    return (
+
+    )
+  }
+}
+export default Search;
