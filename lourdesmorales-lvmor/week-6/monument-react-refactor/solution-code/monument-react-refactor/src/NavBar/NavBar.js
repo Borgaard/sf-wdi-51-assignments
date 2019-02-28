e3ded3122376d41aae0a2347390ea89f12899e9c
lvmor <lvm.lspa@gmail.com>
2019-02-28 13:13:13 -0800

    submitting monument-refactor/solution-code, added routes week 6

--- lourdesmorales-lvmor/week-6/monument-react-refactor/solution-code/monument-react-refactor/src/NavBar/NavBar.js
+++ lourdesmorales-lvmor/week-6/monument-react-refactor/solution-code/monument-react-refactor/src/NavBar/NavBar.js
@@ -1,18 +1,35 @@
-import React from 'react'
-import './NavBar.css'
+import React from "react";
+import "./NavBar.css";
+import Blog from "../Blog/Blog";
+import { Route, Link, Switch } from "react-router-dom";
 
 const navbar = () => {
-    return (
-        <nav>
-		    <a className="hamburger" href=""><i className="fa fa-bars"></i></a>
-		    <ul>
-		        <li><a href="index.html#about">About</a></li>
-		        <li><a href="index.html#gallery">Gallery</a></li>
-		        <li><a href="blog.html">Blog</a></li>
-		        <li><a href="index.html#contact">Contact</a></li>
-		    </ul>
-	    </nav>
-    )
-}
+  return (
+    <nav>
+      <a className="hamburger" href="">
+        <i className="fa fa-bars" />
+      </a>
+      <ul>
+        <li>
+          <a href="index.html#about">About</a>
+        </li>
+        <li>
+          <a href="index.html#gallery">Gallery</a>
+        </li>
+        <li>
+          <Link to="/blog">Blog</Link>
+        </li>
+        <li>
+          <a href="index.html#contact">Contact</a>
+        </li>
+      </ul>
+      <div className="main">
+        <Switch>
+          <Route path="/blog" component={Blog} />
+        </Switch>
+      </div>
+    </nav>
+  );
+};
 
-export default navbar
+export default navbar;
