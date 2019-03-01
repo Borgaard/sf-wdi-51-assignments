e46c3691bdffb6feb36881e44890cdb63e8cf441
nassBouz <bouzianenassima@gmail.com>
2019-03-01 12:24:43 -0800

    added  Todo.js file

---
+++ nassima-nassBouz/week-6/my-react-todo/src/containers/TodosContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TodosContainer extends Component {
+  render() {
+    return (
+      <h2>
+        I am the TodosContainer page
+      </h2>
+    );
+  }
+}
+
+export default TodosContainer;
