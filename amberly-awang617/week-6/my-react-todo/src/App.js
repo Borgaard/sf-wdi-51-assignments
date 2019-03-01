302372eb67117cac3794ce54cae0fb834f9f98d8
awang617 <wanga18@mail.wlu.edu>
2019-03-01 11:44:34 -0800

    fixed typo

--- amberly-awang617/week-6/my-react-todo/src/App.js
+++ amberly-awang617/week-6/my-react-todo/src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
 import Header from './components/Header';
-import Home from './components/Home';
-import TodosContainer from './containers/TodosContainer';
+import MyRoutes from './config/routes'
+// import { Switch, Route } from 'react-router-dom';
+// import Home from './components/Home';
+// import TodosContainer from './containers/TodosContainer';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="container">
 				<Header/>
-				<Switch>
+				{ MyRoutes }
+				{/* {<Switch>
 					<Route exact path='/' component={ Home }/>
 					<Route path='/todos' component={ TodosContainer }/>
-				</Switch>
+				</Switch>} */}
 			</div>
 		);
 	}
