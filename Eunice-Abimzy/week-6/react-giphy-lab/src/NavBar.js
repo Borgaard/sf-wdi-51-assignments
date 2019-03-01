7c3f2494ae92fbc1fe852191e909465bbcd29857
Abimzy <eabiemo@gmail.com>
2019-02-28 19:15:02 -0800

    Created NavBar component

---
+++ Eunice-Abimzy/week-6/react-giphy-lab/src/NavBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './App.css'
+
+ class NavBar extends Component {
+  render() {
+    return (
+      <nav className="Nav">
+          <h2>Giphy Search</h2>
+      </nav>
+    )
+  }
+}
+export default NavBar
