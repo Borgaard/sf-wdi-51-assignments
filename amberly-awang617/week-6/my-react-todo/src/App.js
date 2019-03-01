158eb385821cdb264c16c76454fdf53ed0b495ce
awang617 <wanga18@mail.wlu.edu>
2019-03-01 10:35:20 -0800

    added in Home and Container

--- amberly-awang617/week-6/my-react-todo/src/App.js
+++ amberly-awang617/week-6/my-react-todo/src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
+import TodosContainer from './containers/TodosContainer';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <h1>Hello World</h1>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="container">
+				<Switch>
+					<Route exact path='/' component={ Home }/>
+					<Route path='/todos' component={ TodosContainer }/>
+				</Switch>
+			</div>
+		);
+	}
 }
 
 export default App;
