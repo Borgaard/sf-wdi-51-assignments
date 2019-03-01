3feb198bd1126c9750553bd9536e46205f1d2e5b
awang617 <wanga18@mail.wlu.edu>
2019-03-01 11:46:42 -0800

    make axios call in Todos container

--- amberly-awang617/week-6/my-react-todo/src/containers/TodosContainer.js
+++ amberly-awang617/week-6/my-react-todo/src/containers/TodosContainer.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import TodoModel from '../models/Todo';
 
 class TodosContainer extends Component {
   render() {
+    TodoModel.all().then( (res) => {
+      console.log(res);
+    })
     return (
-      <h2>
-        I am the TodosContainer page
-      </h2>
+      <div className='todosContainter'>
+        <h2>I am the TodosContainer page</h2>
+      </div>
     );
   }
 }
