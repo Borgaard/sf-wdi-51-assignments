eccd893bf5dd4303d26f2ad98c63ca7b65521da8
Matt Freeland <matthewjfreeland@gmail.com>
2019-03-01 11:19:04 -0800

    removes routing from App and creates it in a seperate namespace

--- matt-WalrusMaximus/week-6/my-react-todo/src/App.js
+++ matt-WalrusMaximus/week-6/my-react-todo/src/App.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
-import {
-  Switch,
-  Route 
-} from 'react-router-dom';
-import Home from './components/Home';
-import TodosContainer from './containers/TodosContainer';
+import Header from './components/Header';
+import MyRoutes from './config/routes';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-        <Switch>
-          <Route exact path='/' component={ Home } />
-          <Route path='/todos' component={ TodosContainer } />
-        </Switch>
+        <Header />
+        { MyRoutes }
       </div>
     );
   }
