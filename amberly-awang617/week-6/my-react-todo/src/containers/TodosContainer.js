6be7a06fec8f92fa53d17ce4498cbd2914330893
awang617 <wanga18@mail.wlu.edu>
2019-03-01 12:14:15 -0800

    add all business logic to container, and pass along props to the components

--- amberly-awang617/week-6/my-react-todo/src/containers/TodosContainer.js
+++ amberly-awang617/week-6/my-react-todo/src/containers/TodosContainer.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 import TodoModel from '../models/Todo';
+import TodosList from '../components/TodosList'
 
 class TodosContainer extends Component {
-  render() {
+  state = {
+    todos: []
+  };
+
+  // when TodosContainer mounts, fetch todo data
+  componentDidMount () {
+    this.fetchData()
+  }
+
+  // Fetch all todos from super crud data
+  fetchData () {
     TodoModel.all().then( (res) => {
-      console.log(res);
-    })
+      this.setState({
+        todos: res.data.todos,
+        todo: ''
+      });
+    })    
+  }
+
+  render() {
     return (
       <div className='todosContainter'>
-        <h2>I am the TodosContainer page</h2>
+        <TodosList todos={this.state.todos} />
       </div>
     );
   }
