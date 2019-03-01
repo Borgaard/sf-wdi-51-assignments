6478d6bc85e1c734325c4875e99bc198e9101716
Heggyhere <heggyy@gmail.com>
2019-02-28 23:24:00 -0800

    find out what event fires for userInput debugger;

--- heggy-heggy231/week6/react-giphy/src/components/Search.js
+++ heggy-heggy231/week6/react-giphy/src/components/Search.js
@@ -6,27 +6,48 @@ import axios from 'axios';
 
 class Search extends Component {
   // Search component's initial state. It should have a query value that corresponds to a search term.
+
   state = {
     query: '',
   }
 
-
-
-  axios.get('url')
-  .then((response) => {
-    console.log(response)
-  })
-  .catch((error) => {
-    console.log(error)
-  })
+  // function that is triggered whenever the user submits the Search form
+  searching = (e) => {
+    e.preventDefault();
+    alert('searched!!');
+    console.log("e", e);
+  }
+  // Define a function that updates your query value in state whenever a change is made to the input field. You can do this using an onInput function on your text box, or by using ref.
+  userInput = (e) => {
+    console.log(e);
+    debugger;
+    // this.state.query = 
+  }
+  
+  // axios.get('url')
+  // .then((response) => {
+  //   console.log(response)
+  // })
+  // .catch((error) => {
+  //   console.log(error)
+  // })
 
   render() {
+    // simple form with a single input and submit button.
+    //  inside of return always wrap in one element like div
     return(
       <div>
-
-      <input type="text" placeholder="search" />
-
-      <input type="submit" value="Submit" />
+    {/* inside of form add event listener onSubmit and call searching function */}
+        <form onSubmit={this.searching}>
+          <label>
+            Search: 
+            {/* `this` points to the current obj I am in; Search component 
+          interesting read on this topic: https://medium.com/capital-one-tech/how-to-work-with-forms-inputs-and-events-in-react-c337171b923b
+          */}
+            <input onInput={this.userInput} type="text" placeholder="search any giphy" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     );
   }
