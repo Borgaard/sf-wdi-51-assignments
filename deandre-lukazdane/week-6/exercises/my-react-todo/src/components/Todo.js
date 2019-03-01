a021aec7a004caf9f01ed04259334f11b1e867f2
lukazdane <deandrevidal@aol.com>
2019-03-01 13:48:45 -0800

    create file for creating noew todos

--- deandre-lukazdane/week-6/exercises/my-react-todo/src/components/Todo.js
+++ deandre-lukazdane/week-6/exercises/my-react-todo/src/components/Todo.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Todo extends Component {
   render() {
     return (
-      <li data-todos-index={this.props.todo.id}>
+      <li data-todos-index={this.props.todo._id}>
         <span>{this.props.todo.body}</span>
       </li>
     );
