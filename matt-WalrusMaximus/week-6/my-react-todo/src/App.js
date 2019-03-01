0e9e97273138a98a2360b4faac3f523a9e0cb151
Matt Freeland <matthewjfreeland@gmail.com>
2019-03-01 10:53:38 -0800

    added routes for container and home

--- matt-WalrusMaximus/week-6/my-react-todo/src/App.js
+++ matt-WalrusMaximus/week-6/my-react-todo/src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import {
+  Switch,
+  Route 
+} from 'react-router-dom';
+import Home from './components/Home';
+import TodosContainer from './containers/TodosContainer';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-        <h1>Hello World</h1>
+        <Switch>
+          <Route exact path='/' component={ Home } />
+          <Route path='/todos' component={ TodosContainer } />
+        </Switch>
       </div>
     );
   }
---
+++ matt-WalrusMaximus/week-6/my-react-todo/src/containers/TodosContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TodosContainer extends Component {
+    render() {
+        return (
+            <div>
+                <h2>I am a todos container. Don't look at me, I'm hideous!</h2>
+            </div>
+        );
+    }
+}
+
+export default TodosContainer;
