f151037d518fda1bdd09adbe725d164ca69fd286
Matt Freeland <matthewjfreeland@gmail.com>
2019-03-01 11:25:17 -0800

    adds container class to todos container and updates styling

--- matt-WalrusMaximus/week-6/my-react-todo/src/containers/TodosContainer.js
+++ matt-WalrusMaximus/week-6/my-react-todo/src/containers/TodosContainer.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class TodosContainer extends Component {
     render() {
         return (
-            <div>
+            <div className="todosContainer">
                 <h2>I am a todos container. Don't look at me, I'm hideous!</h2>
             </div>
         );
