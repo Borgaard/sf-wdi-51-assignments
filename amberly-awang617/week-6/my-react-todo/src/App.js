4fc77cf86a5679202d0aa0b79755540fce5d23ec
awang617 <wanga18@mail.wlu.edu>
2019-03-01 10:58:48 -0800

    added a header component

--- amberly-awang617/week-6/my-react-todo/src/App.js
+++ amberly-awang617/week-6/my-react-todo/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Header from './components/Header';
 import Home from './components/Home';
 import TodosContainer from './containers/TodosContainer';
 
@@ -7,6 +8,7 @@ class App extends Component {
 	render() {
 		return (
 			<div className="container">
+				<Header/>
 				<Switch>
 					<Route exact path='/' component={ Home }/>
 					<Route path='/todos' component={ TodosContainer }/>
