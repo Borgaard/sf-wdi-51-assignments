af8c086e75603d3c004cbe303efe403bcf215343
tiffanyteaze <tteaze@gmail.com>
2019-03-01 10:20:58 -0800

    Finished Sprint 2 for React Todo

---
+++ tiffany-tiffanyteaze/week-6/my-react-todo/src/components/Home.js
@@ -0,0 +1,14 @@
+// components/Home.js
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <h2>
+        I am the Home page
+      </h2>
+    );
+  }
+}
+
+export default Home;
---
+++ tiffany-tiffanyteaze/week-6/my-react-todo/src/containers/TodosContainer.js
@@ -0,0 +1,14 @@
+// containers/TodosContainer.js
+import React, { Component } from 'react';
+
+class TodosContainer extends Component {
+  render() {
+    return (
+      <div className="todosContainer">
+        <h2>I am the TodosContainer page</h2>
+      </div>
+    );
+  }
+}
+
+export default TodosContainer;
