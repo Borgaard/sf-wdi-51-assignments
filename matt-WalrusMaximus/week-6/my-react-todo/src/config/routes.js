9958bef36bccf1290d4edc8a6adb642bda23c326
Matt Freeland <matthewjfreeland@gmail.com>
2019-03-01 11:19:41 -0800

    creates header component and update routing

---
+++ matt-WalrusMaximus/week-6/my-react-todo/src/config/routes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from '../components/Home';
+import TodosContainer from '../containers/TodosContainer';
+
+export default (
+    <Switch>
+        <Route exact path='/' component={ Home } />
+        <Route path='/todos' component={ TodosContainer } />
+    </Switch>
+);
