34a5ad79e51ded7239e53c453b81fa90dd12d2ad
Abimzy <eabiemo@gmail.com>
2019-03-01 10:11:36 -0800

    Installed react and created App component

---
+++ Eunice-Abimzy/week-6/my-react-todo/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+ class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Hello World</h1>
+      </div>
+    )
+  }
+}
+export default App
