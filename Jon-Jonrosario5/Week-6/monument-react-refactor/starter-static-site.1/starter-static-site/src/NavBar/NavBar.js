0c71e79061c4a19660d56c3ab3f3439eebcbad29
Jonrosario5 <Jonathan.Rosario550@gmail.com>
2019-03-03 15:36:50 -0800

    Finished adding logic for the routes in the NavBar. Linked the Landing page to the Blog page

--- Jon-Jonrosario5/Week-6/monument-react-refactor/starter-static-site.1/starter-static-site/src/NavBar/NavBar.js
+++ Jon-Jonrosario5/Week-6/monument-react-refactor/starter-static-site.1/starter-static-site/src/NavBar/NavBar.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
 import './NavBar.css';
+import Blog from "../Blog/Blog"
+import {Link} from "react-router-dom";
+
 
 class NavBar extends Component {
     render(){
@@ -7,10 +10,10 @@ class NavBar extends Component {
             <nav>
 		        <a class="hamburger" href=""><i class="fa fa-bars"></i></a>
 		        <ul>
-		            <li><a href="index.html#about">About</a></li>
-		            <li><a href="index.html#gallery">Gallery</a></li>
-		            <li><a href="blog.html">Blog</a></li>
-		            <li><a href="index.html#contact">Contact</a></li>
+		            <li><a href="#about">About</a></li>
+		            <li><a href="#gallery">Gallery</a></li>
+		            <li><Link to = "/blog">Blog</Link></li>
+		            <li><a href="#contact">Contact</a></li>
 		        </ul>
 	        </nav>
         )
