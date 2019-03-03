ca56268a023885b8912642ecee914bce6449d967
Matt Freeland <matthewjfreeland@gmail.com>
2019-03-03 15:55:58 -0800

    index/landing page is functional

--- matt-WalrusMaximus/week-6/monument-react-refactor/starter-static-site/monument-blog/src/App.js
+++ matt-WalrusMaximus/week-6/monument-react-refactor/starter-static-site/monument-blog/src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 import Nav from './components/Nav/Nav'
 import Gallery from './components/Index/Gallery'
-// import Header from '/src/components/Header';
-// import Main from '/src/components/Main';
-// import Footer from '/src/components/Footer';
+import About from './components/Index/About'
+import Contact from './components/Index/Contact';
+import Footer from './components/Footer'
+import Articles from './components/Index/Articles'
+import './components/css/landing.css'
+import './components/css/style.css'
 
 class App extends Component {
   render() {
     return (
       <div className = "app">
         <Nav />
+        <Articles />
+        <About />
         <Gallery />
-        {/* <Index />
-        <Footer /> */}
+        <Contact />
+        <Footer />
       </div>
     );
   }
