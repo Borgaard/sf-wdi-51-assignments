db5963f9c211e72156877fc2fccba3831dacd1c9
vagarioustoast <vagarioustoast@gmail.com>
2019-03-03 11:23:51 -0800

    don't know where things went wrong so starting over.

--- brandon-vagarioustoast/week-6/react-giphy-lab/src/index.js
+++ brandon-vagarioustoast/week-6/react-giphy-lab/src/index.js
@@ -2,6 +2,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Home from "./Home";
 
-ReactDOM.render(<Home />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
