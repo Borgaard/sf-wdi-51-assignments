d8cdece186281c1dffbf1f12d49fba8d9621dbe8
vagarioustoast <vagarioustoast@gmail.com>
2019-03-03 12:20:52 -0800

    added search on form

--- brandon-vagarioustoast/week-6/react-giphy-lab/src/Search.js
+++ brandon-vagarioustoast/week-6/react-giphy-lab/src/Search.js
@@ -1,11 +1,22 @@
 import React from "react";
 
 export default class Search extends React.Component {
+  state = {
+    query: ""
+  };
+
+  onInput = e => {
+    e.preventDefault();
+    console.log("Searched");
+  };
+
   render() {
     return (
       <div>
-        <input type="text" placeholder="Search Here" />
-        <button>Go</button>
+        <form onSubmit={this.onInput}>
+          <input type="text" placeholder="Search Here" />
+          <button>Go</button>
+        </form>
       </div>
     );
   }
