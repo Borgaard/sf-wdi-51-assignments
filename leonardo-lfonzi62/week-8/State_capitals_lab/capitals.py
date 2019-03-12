91910fc0598e9c9f591a4a8f3a0383b4d0650799
lfonzi62 <lfontenette@berkeley.edu>
2019-03-11 21:15:35 -0700

    completed state lab hw

---
+++ leonardo-lfonzi62/week-8/State_capitals_lab/capitals.py
@@ -0,0 +1,204 @@
+import random
+# an array of state dictionaries
+states = [
+{
+    "name": "Alabama",
+    "capital": "Montgomery"
+}, {
+    "name": "Alaska",
+    "capital": "Juneau"
+}, {
+    "name": "Arizona",
+    "capital": "Phoenix"
+}, {
+    "name": "Arkansas",
+    "capital": "Little Rock"
+}, {
+    "name": "California",
+    "capital": "Sacramento"
+}, {
+    "name": "Colorado",
+    "capital": "Denver"
+}, {
+    "name": "Connecticut",
+    "capital": "Hartford"
+}, {
+    "name": "Delaware",
+    "capital": "Dover"
+}, {
+    "name": "Florida",
+    "capital": "Tallahassee"
+}, {
+    "name": "Georgia",
+    "capital": "Atlanta"
+}, {
+    "name": "Hawaii",
+    "capital": "Honolulu"
+}, {
+    "name": "Idaho",
+    "capital": "Boise"
+}, {
+    "name": "Illinois",
+    "capital": "Springfield"
+}, {
+    "name": "Indiana",
+    "capital": "Indianapolis"
+}, {
+    "name": "Iowa",
+    "capital": "Des Moines"
+}, {
+    "name": "Kansas",
+    "capital": "Topeka"
+}, {
+    "name": "Kentucky",
+    "capital": "Frankfort"
+}, {
+    "name": "Louisiana",
+    "capital": "Baton Rouge"
+}, {
+    "name": "Maine",
+    "capital": "Augusta"
+}, {
+    "name": "Maryland",
+    "capital": "Annapolis"
+}, {
+    "name": "Massachusetts",
+    "capital": "Boston"
+}, {
+    "name": "Michigan",
+    "capital": "Lansing"
+}, {
+    "name": "Minnesota",
+    "capital": "St. Paul"
+}, {
+    "name": "Mississippi",
+    "capital": "Jackson"
+}, {
+    "name": "Missouri",
+    "capital": "Jefferson City"
+}, {
+    "name": "Montana",
+    "capital": "Helena"
+}, {
+    "name": "Nebraska",
+    "capital": "Lincoln"
+}, {
+    "name": "Nevada",
+    "capital": "Carson City"
+}, {
+    "name": "New Hampshire",
+    "capital": "Concord"
+}, {
+    "name": "New Jersey",
+    "capital": "Trenton"
+}, {
+    "name": "New Mexico",
+    "capital": "Santa Fe"
+}, {
+    "name": "New York",
+    "capital": "Albany"
+}, {
+    "name": "North Carolina",
+    "capital": "Raleigh"
+}, {
+    "name": "North Dakota",
+    "capital": "Bismarck"
+}, {
+    "name": "Ohio",
+    "capital": "Columbus"
+}, {
+    "name": "Oklahoma",
+    "capital": "Oklahoma City"
+}, {
+    "name": "Oregon",
+    "capital": "Salem"
+}, {
+    "name": "Pennsylvania",
+    "capital": "Harrisburg"
+}, {
+    "name": "Rhode Island",
+    "capital": "Providence"
+}, {
+    "name": "South Carolina",
+    "capital": "Columbia"
+}, {
+    "name": "South Dakota",
+    "capital": "Pierre"
+}, {
+    "name": "Tennessee",
+    "capital": "Nashville"
+}, {
+    "name": "Texas",
+    "capital": "Austin"
+}, {
+    "name": "Utah",
+    "capital": "Salt Lake City"
+}, {
+    "name": "Vermont",
+    "capital": "Montpelier"
+}, {
+    "name": "Virginia",
+    "capital": "Richmond"
+}, {
+    "name": "Washington",
+    "capital": "Olympia"
+}, {
+    "name": "West Virginia",
+    "capital": "Charleston"
+}, {
+    "name": "Wisconsin",
+    "capital": "Madison"
+}, {
+    "name": "Wyoming",
+    "capital": "Cheyenne"
+}]
+
+three_States = [
+{
+    "name": "Alabama",
+    "capital": "Montgomery"
+}, {
+    "name": "Alaska",
+    "capital": "Juneau"
+}, {
+    "name": "Arizona",
+    "capital": "Phoenix"
+}]
+
+# student = {
+#  	'name': 'Fred',
+#  	'course': 'WDI',
+#  	'current_week': 4
+#  } 
+
+# GAME LOGIC STARTS HERE
+
+random.shuffle(states) # python method that shuffles the states list
+print('Are you ready to play the State Capitals game?')
+score = 0   #init score , correct_amount and wrong_amount variables 
+correct_Amount = 0 
+wrong_Amount = 0
+total_Questions = len(states)
+
+for state in states: # loops through the array of objects
+    hint = print('Here is a hint of the state capital: ' + state['capital'][:3]) # prints the first three characters of the state capital
+    user_Guess = input('What is the capital of ' + state['name'] + '?') #capturing user inputs' input
+    if user_Guess == state['capital']: # comparing the user's input to the current state's capital in the list
+        print('Correct!')
+        score += 1
+        correct_Amount += 1 #correct amount is incremented by one for each correct answer
+        print('Your current score is ' + str(score))
+    else:
+        print('Incorrect State Name!')
+        score -= 1
+        print('Your current score is ' + str(score))
+if score < 0: # if the score is negative, set the score to equal 0
+    score = 0       
+print('Congrats, your final score is ' + str(score) + ' !' + ' you got ' + str(correct_Amount) + ' out of ' + str(total_Questions) + ' questions correct')
+
+
+
+
+
+# for key in student:
+#      print( f"{key} = {student[key]}" )
