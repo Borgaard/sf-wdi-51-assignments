56ab33421c416090a5cc22d17fe033229db2d986
Heggyhere <heggyy@gmail.com>
2019-03-12 16:00:10 -0700

    student elf, full_name, reason done

--- heggy-heggy231/week8/codebar-master/codebar.py
+++ heggy-heggy231/week8/codebar-master/codebar.py
@@ -1,7 +1,8 @@
+# Everybody at Codebar -- whether they are attending workshops or teaching them -- is a Member.
 class Member: 
   # fill in info here full_name
   # self only exist inside of class!!
-  def __init__(self, full_name=''):
+  def __init__(self, full_name):
     self.full_name = full_name
   
   # method that introduce themselves
@@ -12,4 +13,16 @@ class Member:
 # heggy = Member('heggy')
 # call method introduce()
 # heggy.introduce() # => Hi, my name is heggy!
+
+# Each Member is also either a Student or an Instructor.
+# [X] create 2 classes inherit from Member
+# Each student has a reason for attending Codebar (e.g., "I've always wanted to make websites!").
+class Student(Member):
+  def __init__(self, full_name, reason):
+    Member.__init__(self, full_name)
+    self.reason = reason
+
+# Each instructor a bio (e.g., "I've been coding in Python for 5 years and want to share the love!").
+# Each instructor also has a set of skills (e.g., ["Python", "Javascript", "C++"]).
+# An instructor can gain a new skill using add_skill.
 
